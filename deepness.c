6c584d13b02061a2cd24506f7e9e497f5e227679
pinchyCZN <pinchyCZN@gmail.com>
2015-11-20 16:06:33 -0600

    updates

--- deepness.c
+++ deepness.c
@@ -1,12 +1,23 @@
 #include <windows.h>
+#include <GL/gl.h>
+#include "glext.h"
 
 HWND hmainwin=0;
+HDC hmaindc=0;
+HGLRC hmainglrc=0;
 LONG winstyle=0;
 RECT winrect={0};
 
-LRESULT CALLBACK dlgproc(HWND hwnd,UINT message,WPARAM wParam,LPARAM lParam)
+LRESULT CALLBACK dlgproc(HWND hwnd,UINT msg,WPARAM wparam,LPARAM lparam)
 {
-	return 0;
+	switch(msg){
+	case WM_COMMAND:
+		switch(LOWORD(wparam)){
+		case 
+		}
+		break;
+	}
+	return DefWindowProc(hwnd,msg,wparam,lparam);
 }
 
 int register_window(HINSTANCE hinstance)
@@ -20,9 +31,11 @@ int register_window(HINSTANCE hinstance)
 	wndclass.hCursor=LoadCursor(0,IDC_ARROW);
 	wndclass.lpszClassName=name;
 	if(RegisterClass(&wndclass)){
-												//0x86CF 0000
-		hmainwin=CreateWindowEx(WS_EX_TOPMOST,name,name,WS_POPUP|WS_CLIPSIBLINGS|WS_CLIPCHILDREN|WS_CAPTION|WS_SYSMENU|WS_THICKFRAME|WS_GROUP|WS_TABSTOP,
-				0,0,400,400,0,0,hinstance,0);
+		int exstyle=0; //WS_EX_TOPMOST;
+		//0x86CF 0000
+		int style=WS_POPUP|WS_CLIPSIBLINGS|WS_CLIPCHILDREN|WS_CAPTION|WS_SYSMENU|WS_THICKFRAME|WS_GROUP|WS_TABSTOP;
+		hmainwin=CreateWindowEx(exstyle,name,name,style,
+				0,0,640,480,0,0,hinstance,0);
 		if(hmainwin){
 			winstyle=GetWindowLong(hmainwin,GWL_STYLE);
 			GetWindowRect(hmainwin,&winrect);
@@ -30,10 +43,74 @@ int register_window(HINSTANCE hinstance)
 		}
 	}
 	return result;
-
+}
+int setup_ogl(HWND hwnd)
+{
+	hmaindc=GetDC(hwnd);
+	if(hmaindc!=0){
+		PIXELFORMATDESCRIPTOR pfd;
+		int pixelfmt;
+		memset(&pfd,0,sizeof(pfd));
+		pfd.nSize=sizeof(pfd);
+		pfd.nVersion=1;
+		pfd.dwFlags=PFD_DOUBLEBUFFER|PFD_DRAW_TO_WINDOW|PFD_SUPPORT_OPENGL;
+		pfd.iPixelType=0;
+		pfd.cColorBits=32;
+		pfd.cRedBits=32;
+		pfd.cRedShift=32;
+		pfd.cGreenBits=32;
+		pfd.cGreenShift=32;
+		pfd.cBlueBits=32;
+		pfd.cBlueShift=32;
+		pfd.cAlphaBits=32;
+		pfd.cAlphaShift=32;
+		pfd.cAccumBits=32;
+		pfd.cAccumRedBits=32;
+		pfd.cAccumGreenBits=32;
+		pfd.cAccumBlueBits=32;
+		pfd.cAccumAlphaBits=32;
+		pfd.cDepthBits=32;
+		pfd.cStencilBits=32;
+		pfd.cAuxBuffers=32;
+		pfd.iLayerType=0;
+		pfd.bReserved=0;
+		pfd.dwLayerMask=0;
+		pfd.dwVisibleMask=0;
+		pfd.dwDamageMask=0;
+		pixelfmt=ChoosePixelFormat(hmaindc,&pfd);
+		SetPixelFormat(hmaindc,pixelfmt,&pfd);
+		hmainglrc=wglCreateContext(hmaindc);
+		wglMakeCurrent(hmaindc,hmainglrc);
+		ShowWindow(hmainwin,SW_SHOW);
+		UpdateWindow(hmainwin);
+		SetFocus(hmainwin);
+		glEnable(GL_TEXTURE_2D);
+		glClearColor(.2,.2,.2,0);
+		glClearDepth(0);
+		glDepthFunc(GL_LESS);
+		glEnable(GL_DEPTH_TEST);
+		glShadeModel(GL_SMOOTH);
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		{
+			RECT rect={0};
+			GetClientRect(hwnd,&rect);
+			glViewport(0,0,rect.right,rect.bottom);
+		}
+	}
+}
+int setup_display(HWND hwnd)
+{
+	RECT rect={0};
+	GetWindowRect(hwnd,&rect);
 }
 int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,PSTR szCmdLine,int iCmdShow)
 {
-	register_window(hInstance);
+	if(register_window(hInstance)){
+		setup_ogl(hmainwin);
+		setup_display(hmainwin);
+	}
 
 }
